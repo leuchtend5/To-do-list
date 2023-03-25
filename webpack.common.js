b15e7d6bd3fff3603b79163783da2ded9f55edf0
leuchtend5 <zxcmj1@gmail.com>
2023-03-25 12:15:58 +0800

    add multiple files

---
+++ webpack.common.js
@@ -0,0 +1,5 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
