01489dd34935552123a31518c8822dc7014735bd
leuchtend5 <zxcmj1@gmail.com>
2023-03-26 23:13:02 +0800

    add more styles

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,33 @@
+import 'regenerator-runtime';
 import '../styles/css/main.css';
+import App from './views/app';
 
 const listName = document.querySelectorAll('li');
+const hamburgerMenuBtn = document.querySelector('.hamburger-menu');
+const menuList = document.querySelector('.all-menu');
+const footerText = document.querySelector('.footer-text');
 
-function limitName() {
-  listName.forEach((list) => {
-    if (list.textContent.length > 15) {
-      list.textContent = `${list.textContent.substring(0, 15)}...`;
+function sampleOnly() {
+  // listName.forEach((list) => {
+  //   if (list.textContent.length > 15) {
+  //     list.textContent = `${list.textContent.substring(0, 15)}...`;
+  //   }
+  // });
+
+  let menuToggle = true;
+
+  hamburgerMenuBtn.addEventListener('click', () => {
+    if (menuToggle) {
+      menuList.classList.add('active');
+      menuToggle = !menuToggle;
+    } else {
+      menuList.classList.remove('active');
+      menuToggle = !menuToggle;
     }
   });
 }
 
-limitName();
+const newDate = new Date().getFullYear();
+footerText.textContent = `Copyright © ${newDate} Leuchtend`;
+
+// sampleOnly();
