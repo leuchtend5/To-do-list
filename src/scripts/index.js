0c43f152219655cb7b544d053377229cfd366cd5
leuchtend5 <zxcmj1@gmail.com>
2023-03-28 16:19:35 +0800

    styling menu list

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,17 +2,18 @@ import 'regenerator-runtime';
 import '../styles/css/main.css';
 import App from './views/app';
 
-const listName = document.querySelectorAll('li');
+const listProjectName = document.querySelectorAll('.project-list .group-name > p');
 const hamburgerMenuBtn = document.querySelector('.hamburger-menu');
 const menuList = document.querySelector('.all-menu');
 const footerText = document.querySelector('.footer-text');
 
 function sampleOnly() {
-  // listName.forEach((list) => {
-  //   if (list.textContent.length > 15) {
-  //     list.textContent = `${list.textContent.substring(0, 15)}...`;
-  //   }
-  // });
+  listProjectName.forEach((list) => {
+    if (list.textContent.length > 25) {
+      list.textContent = `${list.textContent.substring(0, 25)}...`;
+    }
+    console.log(list.textContent.length);
+  });
 
   let menuToggle = true;
 
@@ -30,4 +31,4 @@ function sampleOnly() {
 const newDate = new Date().getFullYear();
 footerText.textContent = `Copyright © ${newDate} Leuchtend`;
 
-// sampleOnly();
+sampleOnly();
